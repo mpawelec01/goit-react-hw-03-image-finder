d10cc6687ccd98846f90c5bf6dc135a8b2398353
Mateusz Pawelec <m.pawelec01@gmail.com>
2023-12-01 09:52:02 +0100

    changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import Searchbar from './Searchbar';
-import css from './styles.css';
+import './styles.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import ImageGallery from './ImageGallery';
